5b1b903d2a1c60d53d5918b72b1e405ca57a244f
FlashUKLTD <ryanmcgall@outlook.com>
2026-03-20 13:51:56 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -190,7 +190,7 @@ window.FLASH_CUSTOM = window.FLASH_CUSTOM || {
         id: "about",
         type: "link",
         match: { idEquals: "header-about" },
-        label: "Inside Flash",
+        label: "Our Impact",
         enabled: true,
         icon:
           '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.8">' +
